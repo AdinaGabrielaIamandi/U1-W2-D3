25fd3e3c4df626168b12f4087a171dacc973e16e
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-08 16:49:25 +0100

    correction ex.

--- D3.js
+++ D3.js
@@ -98,13 +98,24 @@ numeri.sort(function(a, b) {
 });
 console.log(numeri);*/
 
-/*a=2
-b=4
-c=6
-if {
-
+let a = 25;
+let b = 25;
+let c = 25;
+if (a > b && a > c && b > c) {
+    console.log(a, b, c);
+} else if (a > b && a > c && b < c) {
+    console.log(a, c, b);
+} else if (b > a && b > c && a > c) {
+    console.log(b, a, c);
+} else if (b > c && b > a && a < c) {
+    console.log(b, c, a);
+} else if (c > a && c > b && a > b) {
+    console.log(c, a, b);
+} else if (c > a && c > b && a < b) {
+    console.log(c, b, a);
+} else {
+    console.log("Tutti i numeri sono uguali");
 }
-
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
